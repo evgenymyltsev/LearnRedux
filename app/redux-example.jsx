649d50e43b0dac5bbc5e8e5fdd6c0499b7d46be4
Evgeny Myltsev <myltsev.ea@gamil.com>
2017-02-13 14:36:52 +0300

    Add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,13 +2,54 @@ var redux = require('redux');
 
 console.log('starting redux example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault ={
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+};
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action) => {
   // state = state || {name: 'Anonymous'};
   switch (action.type) {
     case 'CHANGE_NAME':
       return {
         ...state,
         name: action.name
+      };
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+      };
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter( (hobby) => hobby.id !== action.id)
+      };
+    case 'ADD_MOVIE':
+      return{
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: nextMovieId++,
+            title: action.title,
+            genre: action.genre
+
+          }
+        ]
+      };
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter( (movie) => movie.id !== action.id)
       };
     default:
       return state;
@@ -24,6 +65,8 @@ var unsubscribe = store.subscribe(() => {
 
   console.log('Name is', state.name);
   document.getElementById('app').innerHTML = state.name;
+
+  console.log('New state', store.getState());
 });
 // unsubscribe();
 
@@ -34,7 +77,39 @@ store.dispatch ({
   name: 'ME'
 });
 
+store.dispatch ({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+});
+
+store.dispatch ({
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+
 store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Emily'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Mad Max',
+  genre: 'Action'
+});
+
+store.dispatch ({
+  type: 'REMOVE_MOVIE',
+  id: 1
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Batman',
+  genre: 'Action'
 });
