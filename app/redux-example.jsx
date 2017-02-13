9d15878537c019911d0cfeab62c3eb3b5d18d21e
Evgeny Myltsev <myltsev.ea@gamil.com>
2017-02-13 16:03:59 +0300

    Add support for location fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
 var redux = require('redux');
+var axios = require('axios');
 
 console.log('starting redux example');
 
@@ -141,10 +142,55 @@ var removeMovie = (id) => {
   };
 };
 
+//Map reducer and action generators
+//----------------------------------
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+  switch (action.type) {
+    case 'START_LOCATION_FETCH':
+      return {
+        isFetching: true,
+        url: undefined
+      };
+    case 'COMPLETE_LOCATION_FETCH':
+      return {
+        isFetching: false,
+        url: action.url
+      };
+    default:
+      return state;
+  }
+};
+
+var startLocationFetch = () => {
+  return {
+    type: 'START_LOCATION_FETCH',
+  };
+};
+
+var completeLocationFetch = (url) => {
+  return {
+    type: 'COMPLETE_LOCATION_FETCH',
+    url
+  };
+};
+
+var fetchLocation = () => {
+  store.dispatch(startLocationFetch());
+
+  axios.get('http://ipinfo.io').then(function (res) {
+    var loc = res.data.loc;
+    var baseUrl = 'http://maps.google.com?q='
+
+    store.dispatch(completeLocationFetch(baseUrl + loc));
+  });
+};
+
+
 var reducer = redux.combineReducers ({
   name: nameReducer,
   hobbies: hobbiesReducer,
-  movies: moviesReducer
+  movies: moviesReducer,
+  map: mapReducer
 });
 
 var store = redux.createStore(reducer, redux.compose(
@@ -159,10 +205,18 @@ var unsubscribe = store.subscribe(() => {
   document.getElementById('app').innerHTML = state.name;
 
   console.log('New state', store.getState());
+
+  if (state.map.isFetching) {
+    document.getElementById('app').innerHtml = 'Loading...'
+  } else if (state.map.url) {
+    document.getElementById('app').innerHTML = '<a href="' + state.map.url + '" target="_blank">View Your Location</a>'
+  }
 });
 // unsubscribe();
 
 var currentState = store.getState();
+
+fetchLocation();
 
 store.dispatch (changeName('ME'));
   // {
